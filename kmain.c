068eccc2240a1bf0c9f889ebdb51917cb9aa2332
Pubudu Wickramathunge <pubuduwickramathunge99@gmail.com>
2021-09-11 01:56:59 +0530

    Page Frame Allocation

--- kmain.c
+++ kmain.c
@@ -3,35 +3,29 @@
 #include "interrupts.h"
 #include "multiboot.h"
 #include "paging.h"
+#include "paging.h"
 
-void init(){
-   segments_install_gdt();
-   interrupts_install_idt();
-   init_paging();
+/* Function to initialize */
+ void init(u32int kernelPhysicalStart, u32int kernelPhysicalEnd) {
+  /* Initialize segment descriptor tables */
+  init_gdt();
+
+  /* Configure serial port */
+  serial_configure(SERIAL_COM1_BASE, Baud_115200);
+  
+  /* Initialize paging */
+  init_paging(kernelPhysicalStart, kernelPhysicalEnd);
+  
+  /* Initialize idt */
+  interrupts_install_idt();
+  
 }
 
-int kmain(unsigned int ebx){
-   
-   init();
-
-   multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
-   multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
-   unsigned int address_of_module = modules->mod_start;
-
-   if((mbinfo->mods_count) == 1){
-      char str[] = "Module Loaded Successfully\n";
-      serial_write(str,sizeof(str));
-      
-      typedef void (*call_module_t)(void);
-         call_module_t start_program = (call_module_t) address_of_module;
-         start_program();
-
-   }
-   else{
-      char str[] = "More than one modules loaded\n";
-      serial_write(str,sizeof(str));
-   }
-
-   return 0;
+/* Kernel Main */
+ s32int kmain(u32int kernelPhysicalStart, u32int kernelPhysicalEnd) {
+	
+    	// Initialize all modules
+   	init(kernelPhysicalStart, kernelPhysicalEnd);
+  	 
+  	return 0;
 }
-
